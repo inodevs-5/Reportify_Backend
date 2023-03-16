a426a81da4cf6bc11cf134d717423ea4deeadef8
Gustavo Ando <gustavo.k.ando@gmail.com>
2023-03-16 16:15:38 -0300

    Adicionando funcionalidade de exibir todos os ROs

--- src/routes/ros.js
+++ src/routes/ros.js
@@ -2,4 +2,6 @@ const router = require("express").Router()
 
 const roController = require("../controllers/roController")
 
+router.route("/").get((req, res) => roController.getAll(req, res))
+
 module.exports = router

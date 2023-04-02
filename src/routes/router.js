a46623e9e46bf9d11a2fbf088227b24b911a83c8
luisouza <luisttine@gmail.com>
2023-04-02 13:36:20 -0300

    Ajustando checagem de token nas rotas

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,10 +2,10 @@ const router = require("express").Router()
 const { checkToken } = require("../middlewares/authMiddleware")
 
 const rosRouter = require("./ros")
-router.use("/ro", rosRouter)
+router.use("/ro", checkToken, rosRouter)
 
 const usuariosRouter = require("./usuarios")
-router.use("/usuario", checkToken, usuariosRouter)
+router.use("/usuario", usuariosRouter)
 
 const loginRouter = require("./login")
 router.use("/login", loginRouter)

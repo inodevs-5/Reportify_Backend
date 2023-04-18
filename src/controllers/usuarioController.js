e6a21bd1b1323bde4d7f036c07c1080a57a5a5c0
Kauã <kaua.reno@gmail.com>
2023-04-18 17:50:51 -0300

    adicionando rotas de cadastro de usuário

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -3,48 +3,40 @@ const bcrypt = require("bcrypt")
 
 const usuarioController = {
     create: async(req, res) => {
+        const {nome, email, perfil, empresa, senha} = req.body
+    
+        // validations
+        if (!nome) {
+            return res.status(422).json({ msg: "Nome é Obrigatorio"})
+        }
+        if (!email) {
+            return res.status(422).json({ msg: "Email é Obrigatorio"})
+        }
+        if(!senha){
+            return res.status(422).json({ msg: "Senha é Obrigatoria"})
+        }
+    
+        // check if user exist
+        const usuarioExists = await Usuario.findOne({ email: email})
+    
+        if (usuarioExists) {
+            return res.status(422).json({ msg: 'Por Favor, utilize outro e-mail'})
+        }
+    
+        // create passwor
+        const salt = await bcrypt.genSalt(12)
+        const senhaHash = await bcrypt.hash(senha, salt)
+    
+        // create user
+        const usuario = new Usuario({
+            nome, 
+            email,
+            perfil,
+            empresa, 
+            senha: senhaHash,
+        })
+    
         try {
-            const {
-                nome,
-                email,
-                perfil,
-                empresa,
-                senha
-            } = req.body
-        
-            // validations
-            if (!nome) {
-                return res.status(422).json({ msg: "Nome é Obrigatorio"})
-            }
-            if (!email) {
-                return res.status(422).json({ msg: "Email é Obrigatorio"})
-            }
-            if (!empresa) {
-                return res.status(422).json({ msg: "O nome da Empresa é Obrigatorio"})
-            }
-            if(!senha){
-                return res.status(422).json({ msg: "Senha é Obrigatoria"})
-            }
-        
-            // check if user exist
-            const usuarioExists = await Usuario.findOne({ email: email})
-        
-            if (usuarioExists) {
-                return res.status(422).json({ msg: 'Por Favor, utilize outro e-mail'})
-            }
-        
-            // create passwor
-            const salt = await bcrypt.genSalt(12)
-            const senhaHash = await bcrypt.hash(senha, salt)
-        
-            // create user
-            const usuario = await Usuario.create({
-                nome, 
-                email,
-                perfil,
-                empresa, 
-                senha: senhaHash,
-            })
             await usuario.save()
     
             res.status(201).json({msg: 'Usuário criado com sucesso'})

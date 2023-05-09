f2d3507a2b74870439b518a3503c16f0fab5c898
DevDre <andrefelipecosta493@gmail.com>
2023-05-09 11:58:55 -0300

    criação de emails

--- src/controllers/notificacaoController.js
+++ src/controllers/notificacaoController.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer')
 const { Usuario } = require("../models/Usuario")
+const RO = require("../models/RO")
 require('dotenv').config()
 
 const sendEmail = () => {
@@ -47,6 +48,10 @@ const emailNotificacao = {
 
     atendido: async (id, adm_id) => {
         try {
+            const { _id, dataRegistro, classDefeito, nome, tituloOcorrencia, descricaoOcorrencia } = req.body
+
+            const ro = await RO.findById(_id);
+
             const usuario = await Usuario.findById(id);
 
             const usuario_adm = await Usuario.findById(adm_id);
@@ -60,7 +65,19 @@ const emailNotificacao = {
                 from: process.env.EMAIL,
                 to: usuario.email,
                 subject: 'teste de envio de email',
-                text: 'RO atendido'
+                html: `
+                    <h1>O RO ${ro._id} </h1>
+                    <p>Você acabou de ser cadastrado no aplicativo Reportify por um administrador do sistema. Após instalar o aplicativo no seu celular, clique <a href="http://reportify-app-inodevs-2023/senha/${usuario._id}">aqui</a> para definir a sua senha e conseguir se autenticar no aplicativo.</p>
+                    <p>Além disso, confira abaixo as suas informações que foram salvas no nosso banco de dados:</p>
+                    <ul>
+                        <li><strong>Nome: </strong>${usuario.nome}</li>
+                        <li><strong>E-mail: </strong>${ro.dataRegistro}</li>
+                        <li><strong>Empresa: </strong>${ro.classDefeito}</li>
+                        <li><strong>Contato da Empresa: </strong>${ro.tituloOcorrencia}</li>
+                        <li><strong>Contato da Empresa: </strong>${ro.descricaoOcorrencia}</li>
+                    </ul>
+                    <p>Estes são seus dados no sistema. Em relação a sua senha que ainda será definida por você, ela será completamente criptografada antes de ser armazenada. Caso queira alterar qualquer informação que está no nosso banco de dados, você pode enviar um e-mail de solicitação para esse mesmo endereço.</p>
+                `
             }
 
             const RoAtendidoAdm = {

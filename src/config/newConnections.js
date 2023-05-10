7d03fc7f27764075e53c1d92e542e888e04bbf64
Vitória Moura <vim.saturnino@gmail.com>
2023-05-10 13:50:51 -0300

    fix: multiple connections on MongoDB

--- src/config/newConnections.js
+++ src/config/newConnections.js
@@ -1,35 +1,12 @@
 const mongoose = require('mongoose');
 
-// botei essa linha aqui mas não sei se é o lugar certo, mas o erro sumiu =/
-mongoose.set("strictQuery", true)
+mongoose.set("strictQuery", true);
 
-// estou confusa com as variáveis de ambiente, não sei se é essa mesmo aqui
-mongoose.connect(process.env.DB_URL_MAIN, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-})
-.then(conn => {
-  console.log('MongoDb Main Conected')
-})
-.catch( error => {
-  console.log("Error:" + error.message)
-})
-
-mongoose.main = mongoose.createConnection(process.env.DB_URL_MAIN, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-})
-
-mongoose.crypto = mongoose.createConnection(process.env.DB_URL_SECOND_DB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-})
+try {
+  mongoose.main = mongoose.createConnection(process.env.DB_URL_MAIN);
+  mongoose.crypto = mongoose.createConnection(process.env.DB_URL_SECOND_DB);
+} catch (error) {
+  console.error('Erro ao conectar ao banco de dados:', error);
+}
 
 module.exports = mongoose;
-
-// dei um require do conn no cryptoModel e no Usuario.js (model)
-// comentei as linhas da antiga conexão no server.js
-// se for testar lembra de tirar o checkAdmin da rota do usuario e o checkToken do router.js

273b7a15d7705a542bf258cae7d4f3f81855a9a5
luisouza <luisttine@gmail.com>
2023-05-29 08:55:57 -0300

    adding hours in file name for backup

--- src/middlewares/backup.js
+++ src/middlewares/backup.js
@@ -8,6 +8,9 @@ let mongoToolsPath = 'C:\\Program Files\\MongoDB\\Tools\\100\\bin'
 // URI do banco de dados
 let dbUrl = 'mongodb+srv://admin:admin@cluster0.jkblaoo.mongodb.net/api'
 
+// URI do banco de dados de chaves
+// let dbUrlKeys = process.env.DB_URL_SECOND_DB
+
 // função principal que cria o backup
 const dbBackup = () => {
 
@@ -16,18 +19,24 @@ const dbBackup = () => {
   
   // utilizando data atual como nome do arquivo de backup
   const currentDate = new Date()
-  var backupDirName = currentDate.getDate() + '-' + (currentDate.getMonth() + 1) + '-' + currentDate.getFullYear() + '-' + currentDate.getMinutes() +  '.gzip';
+  var backupDirName = 'data_' + currentDate.getDate() + '-' + (currentDate.getMonth() + 1) + '-' + currentDate.getFullYear() + '-' + currentDate.getHours() + '-' + currentDate.getMinutes() +  '.gzip';
+  // var backupKeysDirName = 'keys' + currentDate.getDate() + '-' + (currentDate.getMonth() + 1) + '-' + currentDate.getFullYear() + '-' + currentDate.getHours() + '-' + currentDate.getMinutes() +  '.gzip';
 
   // criando pasta que será salvo o backup
   const backupDirPath = path.join(__dirname + '\\backup', backupDirName);
 
   // código no cmd que cria o backup
   let cmd = `cd ${mongoToolsPath} & mongodump --uri=${dbUrl} --archive="${backupDirPath}" --gzip`;
+  // let cmdKeys = `cd ${mongoToolsPath} & mongodump --uri=${dbUrlKeys} --archive="${backupDirPath}" --gzip`;
 
   // executando o código anterior e exibindo mensagens de erro ou sucesso
   exec(cmd, (error, stdout, stderr) => {
-    console.log([cmd, error, backupDirPath]);
-    console.log("Backup criado com sucesso!");
+    if(error){
+      console.log([cmd, error, backupDirPath]);
+      // console.log([cmdKeys, error]);
+    }else{
+      console.log([error, cmd, backupDirPath],"\nBackup criado com sucesso!")
+    }  
   });
 };
 
